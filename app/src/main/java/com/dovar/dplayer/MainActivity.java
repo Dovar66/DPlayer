7f3655294af9053530dbfe75b9e090b20016b1c2
Dovar66 <xiaohe0949@163.com>
2017-09-10 23:47:33 +0800

    Initial commit

---
+++ app/src/main/java/com/dovar/dplayer/MainActivity.java
@@ -0,0 +1,264 @@
+package com.dovar.dplayer;
+
+import android.app.Dialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lantouzi.wheelview.WheelView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ImageView navImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setupDrawerHeader();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setupDrawerHeader() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View header = navigationView.getHeaderView(0);
+        navImageView = (ImageView) header.findViewById(R.id.nav_image_view);
+        if (navImageView != null) {
+            navImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            showSleepDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void invalidateDrawerImg() {
+//        try {
+//            Bitmap croppedBmp = Bitmap.createBitmap(playerFragment.mVisualizerView.bmp, 0, (int) (75 * ratio), screen_width, screen_width);
+//            navImageView.setImageBitmap(croppedBmp);
+//        } catch (Exception | OutOfMemoryError e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    boolean isSleepTimerEnabled = false;
+    boolean isSleepTimerTimeout = false;
+    long timerSetTime = 0;
+    int timerTimeOutDuration = 0;
+    Handler sleepHandler;
+
+    public void showSleepDialog() {
+        List<String> minuteList = new ArrayList<>();
+        for (int i = 1; i < 25; i++) {
+            minuteList.add(String.valueOf(i * 5));
+        }
+
+        sleepHandler = new Handler();
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.sleep_timer_dialog);
+
+        final WheelView wheelPicker = (WheelView) dialog.findViewById(R.id.wheelPicker);
+        wheelPicker.setItems(minuteList);
+
+        Button setBtn = (Button) dialog.findViewById(R.id.set_button);
+        Button cancelBtn = (Button) dialog.findViewById(R.id.cancel_button);
+        final Button removerBtn = (Button) dialog.findViewById(R.id.remove_timer_button);
+
+        final LinearLayout buttonWrapper = (LinearLayout) dialog.findViewById(R.id.button_wrapper);
+
+        final TextView timerSetText = (TextView) dialog.findViewById(R.id.timer_set_text);
+
+//        setBtn.setBackgroundColor(themeColor);
+//        removerBtn.setBackgroundColor(themeColor);
+        cancelBtn.setBackgroundColor(Color.WHITE);
+
+        if (isSleepTimerEnabled) {
+            wheelPicker.setVisibility(View.GONE);
+            buttonWrapper.setVisibility(View.GONE);
+            removerBtn.setVisibility(View.VISIBLE);
+            timerSetText.setVisibility(View.VISIBLE);
+
+            long currentTime = System.currentTimeMillis();
+            long difference = currentTime - timerSetTime;
+
+            int minutesLeft = (int) (timerTimeOutDuration - ((difference / 1000) / 60));
+            if (minutesLeft > 1) {
+                timerSetText.setText("Timer set for " + minutesLeft + " minutes from now.");
+            } else if (minutesLeft == 1) {
+                timerSetText.setText("Timer set for " + 1 + " minute from now.");
+            } else {
+                timerSetText.setText("Music will stop after completion of current song");
+            }
+
+        } else {
+            wheelPicker.setVisibility(View.VISIBLE);
+            buttonWrapper.setVisibility(View.VISIBLE);
+            removerBtn.setVisibility(View.GONE);
+            timerSetText.setVisibility(View.GONE);
+        }
+
+        removerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSleepTimerEnabled = false;
+                isSleepTimerTimeout = false;
+                timerTimeOutDuration = 0;
+                timerSetTime = 0;
+                sleepHandler.removeCallbacksAndMessages(null);
+                Toast.makeText(getApplicationContext(), "Timer removed", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        setBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSleepTimerEnabled = true;
+                int minutes = Integer.parseInt(wheelPicker.getItems().get(wheelPicker.getSelectedPosition()));
+                timerTimeOutDuration = minutes;
+                timerSetTime = System.currentTimeMillis();
+                sleepHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        isSleepTimerTimeout = true;
+//                        if (playerFragment.mMediaPlayer == null || !playerFragment.mMediaPlayer.isPlaying()) {
+//                            main.runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    Toast.makeText(ctx, "Sleep timer timed out, closing app", Toast.LENGTH_SHORT).show();
+//                                    if (playerFragment != null && playerFragment.timer != null)
+//                                        playerFragment.timer.cancel();
+//                                    notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//                                    try {
+//                                        notificationManager.cancel(1);
+//                                    } catch (Exception e) {
+//                                        e.printStackTrace();
+//                                    } finally {
+//                                        finish();
+//                                    }
+//                                }
+//                            });
+//                        }
+                    }
+                }, minutes * 60 * 1000);
+                Toast.makeText(getApplicationContext(), "Timer set for " + minutes + " minutes", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSleepTimerEnabled = false;
+                isSleepTimerTimeout = false;
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+}
---
+++ app/src/main/java/com/dovar/dplayer/customview/VisualizerView.java
@@ -0,0 +1,70 @@
+package com.dovar.dplayer.customview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017-09-06.
+ */
+
+public class VisualizerView extends View {
+    public static Paint mForePaint = new Paint();
+    public static Paint mForePaint1 = new Paint();
+    public static float width, height, angle, color, lnDataDistance, distance, size, volume, power, outerRadius, alpha;
+    public static float normalizedPosition;
+    public static double LOG_MAX = Math.log(64);
+    public static double TAU = Math.PI * 2;
+    public static double MAX_DOT_SIZE = 0.5;
+    public static double BASE = Math.log(4) / LOG_MAX;
+    public static int w;
+    public static int h;
+    public static Bitmap.Config conf;
+    public static Bitmap bmp;
+
+    public VisualizerView(Context context) {
+        super(context);
+        init();
+    }
+
+    public VisualizerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public VisualizerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mForePaint.setStrokeWidth(1f);//描边
+        mForePaint.setAntiAlias(true);//抗锯齿
+        mForePaint.setColor(Color.rgb(0, 128, 255));
+        mForePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));//绘图模式
+        mForePaint1.setStrokeWidth(1f);
+        mForePaint1.setAntiAlias(true);
+        mForePaint1.setColor(Color.rgb(255, 128, 0));
+    }
+
+    public void updateVisualizer(byte[] bytes) {
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        width = canvas.getWidth();
+        height = canvas.getHeight();
+
+        // Redraw previous points
+        if (bmp != null)
+            canvas.drawBitmap(bmp, 0, 0, null);
+    }
+}
